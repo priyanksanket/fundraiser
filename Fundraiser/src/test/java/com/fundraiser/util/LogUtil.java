b383745fe192a32a4214890148eb0e67709d4964
Priyank Sanket <157965999+priyanksanket@users.noreply.github.com>
2024-12-30 22:55:53 +0530

    Delete Fundraiser directory

--- Fundraiser/src/test/java/com/fundraiser/util/LogUtil.java
+++
@@ -1,9 +0,0 @@
-package com.fundraiser.util;
-
-import org.apache.log4j.Logger;
-
-public class LogUtil {
-    public static Logger getLogger(Class<?> clazz) {
-        return Logger.getLogger(clazz);
-    }
-}
